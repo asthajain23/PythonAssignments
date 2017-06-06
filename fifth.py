0e943b1735484439d436407526753856342cc9a1
asthajain23 <asthajain2308@gmail.com>
2017-06-06 17:02:46 +0530

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -1,7 +1,7 @@
 #Define a function that can accept two strings as input and concatenate them and then print it in console
 
 def printValue(s1,s2):
-print s1+s2
+ print s1+s2
 printValue("astha","jain")
 
 '''Output

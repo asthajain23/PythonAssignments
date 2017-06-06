7c6a7ec61702e5830b918e5075e1e702bc681385
asthajain23 <asthajain2308@gmail.com>
2017-06-06 16:45:33 +0530

    Create tenth.py

---
+++ tenth.py
@@ -0,0 +1,13 @@
+#Define a function that computes the length of a given list or string.
+
+def length(object):#Define the length calculation function
+    count = 0      
+    object =raw_input("Enter the string")
+    for i in object:
+        count=count+1
+    print("Length of the string is"+str(count))length(object)
+
+'''Output
+python tenth.py
+Enter the stringasthaj
+6'''

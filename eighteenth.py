d946519395e2403ce938aec24f8533ef03e8b5f8
asthajain23 <asthajain2308@gmail.com>
2017-06-06 16:52:03 +0530

    Create eighteenth.py

---
+++ eighteenth.py
@@ -0,0 +1,12 @@
+#Write a program which accepts a string from console and print it in reverse order
+
+str = raw_input("Enter your input: ")
+print "Received input is : ", str
+str=str[::-1]
+print str
+
+
+'''Output
+python eighteenth.py
+Enter your input: asthajain
+Received input is :  asthajainniajahtsa'''

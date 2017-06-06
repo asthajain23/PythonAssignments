a496f04ac58894bc93dc4bae8b35e00381d9e427
asthajain23 <asthajain2308@gmail.com>
2017-06-06 16:44:41 +0530

    Create ninth.py

---
+++ ninth.py
@@ -0,0 +1,16 @@
+#Define a function max_of_three() that takes three numbers as arguments and returns the largest of them
+
+def biggest (a, b, c):
+	imax = a
+	if (b > imax) and (b > c):
+		imax = b    
+	elif (c > imax):
+		imax = c    
+	return imaxprint 
+
+print (biggest(5,6,7))
+
+
+'''Output
+python ninth.py
+7'''

60319cf2eeca74111ece0d317dad399ccdc0911c
asthajain23 <asthajain2308@gmail.com>
2017-06-06 16:42:12 +0530

    Create fifth.py

---
+++ fifth.py
@@ -0,0 +1,9 @@
+#Define a function that can accept two strings as input and concatenate them and then print it in console
+
+def printValue(s1,s2):
+print s1+s2
+printValue("astha","jain")
+
+'''Output
+python fifth.py
+asthajain '''

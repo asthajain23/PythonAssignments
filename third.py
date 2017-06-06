82044b65b977ae499db05a5a42ad46f63c7e6355
asthajain23 <asthajain2308@gmail.com>
2017-06-06 18:30:22 +0530

    Delete third.py

--- third.py
+++
@@ -1,6 +0,0 @@
-#Write a python standalone program for:
-             a. insert some values [eg. employee details]in db table.
-             b. fetch the same data and print it on standard output.
-             c. update specific employee info.
-             d. delete specific employee and all the info.
-

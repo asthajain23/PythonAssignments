6c84a0f02db8ee604553a057e5072766d4469d04
asthajain23 <asthajain2308@gmail.com>
2017-06-06 16:39:33 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,14 @@
+#Write a python script which will display system name ,node name and release details of current system
+
+import socket
+import platform
+print(socket.gethostname())
+print(platform.platform())
+print(platform.release())
+
+#Output
+'''
+python second.py
+demo1
+Linux-3.10.0-514.21.1.el7.x86_64-x86_64-with-centos-7.3.1611-Core
+3.10.0-514.21.1.el7.x86_64 '''

a16fc62d932b06c62a39325f2df9af4152cb918a
asthajain23 <asthajain2308@gmail.com>
2017-06-07 11:13:31 +0530

    Update eleventh.py

--- eleventh.py
+++ eleventh.py
@@ -1,8 +1,8 @@
 #.Write a program that accepts a sentence and calculate the number of letters and digits.
-              Suppose the following input is supplied to the program:
+            '''  Suppose the following input is supplied to the program:
               i/p: Hello Priya 1287
               o/p: LETTERS 10
-                     DIGITS 4
+                     DIGITS 4 '''
 
 s = raw_input("Input a string\n")
 d=l=0
